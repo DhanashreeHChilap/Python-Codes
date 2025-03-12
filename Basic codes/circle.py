c0b045236af71194b56619d8136c8ec6c7826be9
DhanashreeHChilap <dhanashreehchilap@gmail.com>
2025-03-12 17:59:20 +0530

    Area and Circumference of a circle code

---
+++ Basic codes/circle.py
@@ -0,0 +1,6 @@
+
+r = int(input("Enter the radius of a circle:"))
+area = 3.14*r*r                                   #Area of a circle
+circumference = 2*3.14*r                          #Circumference of a circle
+print("Area of circle with radius {r} is:",area)
+print("Circumference of circle with radius {r} is:",circumference)
